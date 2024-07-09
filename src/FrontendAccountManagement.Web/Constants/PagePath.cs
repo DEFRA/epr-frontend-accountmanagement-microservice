6902e6297220640e1ef752e6873fc29b3a591dbc
MasilEviden <masil.puveendran@eviden.com>
2024-07-09 09:31:38 +0100

    Added page - check-your-details

--- src/FrontendAccountManagement.Web/Constants/PagePath.cs
+++ src/FrontendAccountManagement.Web/Constants/PagePath.cs
@@ -9,6 +9,7 @@ public static class PagePath
     public const string TeamMemberDetails = "team-member-details";
     public const string RemoveTeamMember = "remove-team-member";
     public const string PreRemoveTeamMember = "pre-remove-team-member";
+    public const string CheckYourDetails = "check-your-details";
 
     // Change role journey paths
     public const string ChangeAccountPermissions = "change-account-permissions";
