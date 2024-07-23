2ab73f1b395399c0842c27eb736fe1cbba221ab8
MasilEviden <156194655+MasilEviden@users.noreply.github.com>
2024-07-23 18:10:49 +0100

    Feature/404751 check personaldetails changes2 (#39)
    * Added unit test
    
    * Using ServiceRoles.BasicUser constant
    
    * Json.JsonSerializer.Deserialize exception logged
    
    * Fix the back link on the "check-your-details" page when coming from the "what-are-your-details" page
    
    * Fix broken unit test
    
    * Add unit tests for the back link fix
    
    * Unit test for Post method CheckYourDetails
    
    * Passing TempData from checkyourdetail to EditUserDetails
    
    * Amended unit test CheckYourDetails
    
    ---------
    
    Co-authored-by: Tim Powell <timothy.powell@eviden.com>

---
+++ src/FrontendAccountManagement.Web.UnitTests/Controllers/AccountManagement/CheckYourDetailsTests.cs
@@ -0,0 +1,129 @@
+﻿using EPR.Common.Authorization.Models;
+using EPR.Common.Authorization.Models;
+using FrontendAccountManagement.Core.Sessions;
+using FrontendAccountManagement.Core.Sessions;
+using FrontendAccountManagement.Web.ViewModels.AccountManagement;
+using FrontendAccountManagement.Web.ViewModels.AccountManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+
+namespace FrontendAccountManagement.Web.UnitTests.Controllers.AccountManagement
+{
+    /// <summary>
+    /// Test the CheckYourDetails functionality
+    /// </summary>
+    [TestClass]
+    public class CheckYourDetailsTests : AccountManagementTestBase
+    {
+        private UserData _userData;
+        private JourneySession _journeySession;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _userData = new UserData
+            {
+                FirstName = "TestFirst",
+                LastName = "TestLast",
+            };
+
+            SetupBase(_userData);
+
+            _journeySession = new JourneySession
+            {
+                UserData = _userData,
+                AccountManagementSession = new AccountManagementSession() { Journey = new List<string>() }
+            };
+
+            SessionManagerMock.Setup(sm => sm.GetSessionAsync(It.IsAny<ISession>()))
+                .Returns(Task.FromResult(_journeySession)
+                   );
+        }
+
+        [TestMethod]
+        public async Task CheckYourDetails_ShouldReturnViewWithExpectedModel()  
+        {
+            // Act
+            var result = await SystemUnderTest.CheckYourDetails();
+
+            // Assert
+            var viewResult = result as ViewResult;
+            viewResult.Should().NotBeNull();
+            var model = viewResult.Model as EditUserDetailsViewModel;
+            model.Should().NotBeNull();
+            model.FirstName.Should().Be("TestFirst");
+            model.LastName.Should().Be("TestLast");
+        }
+
+        [TestMethod]
+        public async Task CheckYourDetailsPost_ShouldReturnActionName() 
+        {
+            // Act        
+            var editUserDetailsViewModel = new EditUserDetailsViewModel
+            {
+                FirstName = "TestFirst",
+                LastName = "TestLast",
+            };
+
+            var result = await SystemUnderTest.CheckYourDetails(editUserDetailsViewModel);
+
+            // Assert
+            result.Should().NotBeNull();
+            ((RedirectToActionResult)result).ActionName.Should().NotBeNull();
+        }
+
+        /// <summary>
+        /// Checks that the journey history is updated in the expected way when navigating from different pages.
+        /// </summary>
+        [TestMethod]
+        [DynamicData(nameof(CheckYourDetailsData))]
+        public async Task CheckYourDetails_DontAddToJourneyOnRefresh(List<string> input, List<string> expected)
+        {
+            // Arrange
+            _journeySession.AccountManagementSession.Journey = input;
+
+            // Act
+            SystemUnderTest.CheckYourDetails();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, _journeySession.AccountManagementSession.Journey);
+        }
+
+        #region Private
+        
+        private static IEnumerable<object[]> CheckYourDetailsData
+        {
+            get
+            {
+                return new[]
+                {
+                    // Check journey is added to correctly when navigating from the management page.
+                    new[]
+                    {
+                        new List<string> { "manage" },
+                        new List<string> { "manage", "check-your-details" },
+                    },
+
+                    // Check the journey is added to correctly when navigating from the "what-are-your-details" page.
+                    new[]
+                    {
+                        new List<string> { "manage", "check-your-details", "what-are-your-details" },
+                        new List<string> { "manage", "check-your-details", "what-are-your-details", "check-your-details" },
+                    },
+
+                    // Check the journey isn't changed when the user refreshes the page.
+                    new[] 
+                    {
+                        new List<string> { "manage", "check-your-details" }, 
+                        new List<string> { "manage", "check-your-details" },
+                    }
+                };
+            }
+        }
+
+        #endregion
+    }
+}
