ffa76362d6956f4bce0c632e9e76f8cee2a52c5e
Ehsan Hatami <48552884+Ehsan-Hatami@users.noreply.github.com>
2024-07-23 16:11:12 +0100

    Feature/406166 uk nation (#38)
    * initiation
    
    * hook up facade endpoint to FE
    
    * wire up to facade layer
    
    * cleanup
    
    * cleanup from extra models
    
    * use resource
    
    * fix view
    
    * cleanup models
    
    * rollback
    
    * add space
    
    * cleanup facade service
    
    * cleanup facade service 2
    
    * add automapper and entities
    
    * wireup to facade service
    
    * refactor
    
    * fix back button
    
    * refactor and add unittest
    
    * use config for url
    
    * add mapper unittest
    
    * fix company house number
    
    * initiation
    
    * use session for companies house data
    
    * refactor
    
    * add unitTests
    
    * fix externalUrl and backbutton text
    
    * add spaceholder for checkCompaniesHouseDetails
    
    * refactor
    
    * fix tests
    
    * refactor the backlink
    
    * rename compnaiesHouseSession property name
    
    ---------
    
    Co-authored-by: Ehsan.Hatami <ehsan.hatami@oceantg.com>
    Co-authored-by: Ehsan Hatami <ehsan.hatamialamdari.external@eviden.com>

---
+++ src/FrontendAccountManagement.Core/Sessions/CompaniesHouseSession.cs
@@ -0,0 +1,8 @@
+﻿using FrontendAccountManagement.Core.Models.CompaniesHouse;
+
+namespace FrontendAccountManagement.Core.Sessions;
+
+public record CompaniesHouseSession
+{
+    public CompaniesHouseResponse CompaniesHouseData { get; set; }
+}
---
+++ src/FrontendAccountManagement.Web/ViewModels/AccountManagement/UkNationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FrontendAccountManagement.Web.Constants.Enums;
+
+namespace FrontendAccountManagement.Web.ViewModels.AccountManagement;
+
+public class UkNationViewModel
+{
+    [Required]
+    public UkNation? UkNation { get; set; }
+}
