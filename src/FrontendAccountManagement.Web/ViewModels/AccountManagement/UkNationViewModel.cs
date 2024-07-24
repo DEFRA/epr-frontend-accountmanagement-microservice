9ba712d0691d849380b9db383c2a022557beba72
Nicholas Peters <nicholas.peters.external@eviden.com>
2024-07-24 14:27:29 +0100

    Fix some minor bugs with back button, and improve error handling code

--- src/FrontendAccountManagement.Web/ViewModels/AccountManagement/UkNationViewModel.cs
+++ src/FrontendAccountManagement.Web/ViewModels/AccountManagement/UkNationViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using FrontendAccountManagement.Web.Constants.Enums;
+using ViewResources = FrontendAccountManagement.Web.Resources.Views;
 
 namespace FrontendAccountManagement.Web.ViewModels.AccountManagement;
 
 public class UkNationViewModel
 {
-    [Required]
+    [Required(ErrorMessageResourceName = "UkNation_ErrorMessage", ErrorMessageResourceType = typeof(ViewResources.AccountManagement.UkNation))]
     public UkNation? UkNation { get; set; }
 }
