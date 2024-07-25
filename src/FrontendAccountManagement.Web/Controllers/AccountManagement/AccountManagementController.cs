daddf1a8d504d3745aabe5616f9b77f14ebdf248
mkeviden <156194524+mkeviden@users.noreply.github.com>
2024-07-25 16:03:41 +0100

    # 411784  update to epr common authorization nuget package : 1.0.15-rc.626876 update to account managment authorize policy and AccountPermissionManagementPolicy to suppport basic user

--- src/FrontendAccountManagement.Web/Controllers/AccountManagement/AccountManagementController.cs
+++ src/FrontendAccountManagement.Web/Controllers/AccountManagement/AccountManagementController.cs
@@ -129,7 +129,8 @@ public class AccountManagementController : Controller
             var roleInOrganisation = userAccount.RoleInOrganisation;
             model.ServiceRoleKey = $"{serviceRoleEnum.ToString()}.{roleInOrganisation}";
             model.OrganisationType = userOrg.OrganisationType;
-            model.HasPermissionToChangeCompany = HasPermissionToChangeCompany(userAccount);
+            model.HasPermissionToChangeCompany = HasPermissionToChangeCompany(session.UserData);
+            model.IsBasicUser = IsBasicUser(session.UserData);
         }
         return View(nameof(ManageAccount), model);
     }
@@ -892,9 +893,8 @@ public class AccountManagementController : Controller
         {
             return IsRegulatorAdmin(userData);
         }
-
         // regulator users cannot view if producer deployment
-        return !IsRegulatorUser(userData);
+        return !IsRegulatorUser(userData) || IsBasicUser(userData);
     }
 
     private static bool IsRegulatorAdmin(UserData userData) =>
@@ -905,6 +905,9 @@ public class AccountManagementController : Controller
 
     private static bool IsRegulatorUser(UserData userData) =>
         IsRegulatorAdmin(userData) || IsRegulatorBasic(userData);
+
+    private static bool IsBasicUser(UserData userData) =>
+       userData.ServiceRoleId == (int)Core.Enums.ServiceRole.Basic;
 
     private static bool HasPermissionToChangeCompany(UserData userData)
     {
@@ -918,4 +921,5 @@ public class AccountManagementController : Controller
         }
         return false;
     }
+
 }
