956b5635898cd75bc2163d35592c1b3d5e2994c1
MasilEviden <masil.puveendran@eviden.com>
2024-07-26 12:53:03 +0100

    Addded Call Api method

--- src/FrontendAccountManagement.Core/Services/IFacadeService.cs
+++ src/FrontendAccountManagement.Core/Services/IFacadeService.cs
@@ -26,8 +26,8 @@ public interface IFacadeService
     Task<EndpointResponseStatus> RemoveUserForOrganisation(string personExternalId, string organisationId, int serviceRoleId);
 
     Task<UserAccountDto?> GetUserAccount();
-	
-	Task<List<int>> GetNationIds(Guid organisationId);
+
+    Task<List<int>> GetNationIds(Guid organisationId);
 
     Task<CompaniesHouseResponse> GetCompaniesHouseResponseAsync(string companyHouseNumber);
 
@@ -39,4 +39,12 @@ public interface IFacadeService
     /// <param name="nationId">The nation id to use</param>
     /// <returns>An async task</returns>
     Task UpdateNationIdByOrganisationId(Guid organisationId, int nationId);
+
+    /// <summary>
+    /// Request the facade to update the user details for a give user ID
+    /// </summary>
+    /// <param name="userId">The ID of the user</param>
+    /// <param name="userDetailsDto">The details to update</param>
+    /// <returns>An async task</returns>
+    Task UpdateUserDetails(Guid? userId, UserDetailsDto userDetailsDto);
 }
