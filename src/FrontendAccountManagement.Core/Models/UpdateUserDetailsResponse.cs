9dbfad64336c160bbbd8b21b748537f37a06a9a1
mkeviden <156194524+mkeviden@users.noreply.github.com>
2024-08-08 15:15:09 +0100

    #423472 ,#410511

---
+++ src/FrontendAccountManagement.Core/Models/UpdateUserDetailsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FrontendAccountManagement.Core.Models
+{
+    /// <summary>
+    /// User details that can be updated
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class UpdateUserDetailsResponse
+    {
+        public bool HasTelephoneOnlyUpdated { get; set; } = false;
+        public bool HasBasicUserDetailsUpdated { get; set; } = false;
+        public bool HasApprovedOrDelegatedUserDetailsSentForApproval { get; set; } = false;
+    }
+}
