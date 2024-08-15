dacc3b9a2c06ca9e3fa35fd61b8f9a413d5eb8e0
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-08-15 16:47:11 +0100

    [feature/412221: WIP fixing failing tests due to new change]

--- src/FrontendAccountManagement.Web.UnitTests/Controllers/AccountManagement/AccountManagementTests.cs
+++ src/FrontendAccountManagement.Web.UnitTests/Controllers/AccountManagement/AccountManagementTests.cs
@@ -512,7 +512,7 @@ public class AccountManagementTests : AccountManagementTestBase
 
         // Assert
         result.Should().BeOfType<UnauthorizedResult>();
-        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Once);
+        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Never);
     }
 
     [TestMethod]
@@ -532,7 +532,7 @@ public class AccountManagementTests : AccountManagementTestBase
 
         // Assert
         result.Should().BeOfType<NotFoundResult>();
-        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Once);
+        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Never);
     }
 
     [TestMethod]
@@ -553,7 +553,7 @@ public class AccountManagementTests : AccountManagementTestBase
 
         // Assert
         result.Should().BeOfType<NotFoundResult>();
-        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Once);
+        SessionManagerMock.Verify(m => m.GetSessionAsync(It.IsAny<ISession>()), Times.Never);
     }
 
     [TestMethod]
