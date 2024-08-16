4c0f38beeb7cfeda23267c3345cdebeb2d9d8931
mkeviden <156194524+mkeviden@users.noreply.github.com>
2024-08-16 12:08:41 +0100

    refactro to unit test for utc

--- src/FrontendAccountManagement.Web.UnitTests/ViewModels/DetailsChangeRequestedViewModelTests.cs
+++ src/FrontendAccountManagement.Web.UnitTests/ViewModels/DetailsChangeRequestedViewModelTests.cs
@@ -1,5 +1,6 @@
 ﻿using FrontendAccountManagement.Web.Extensions;
 using FrontendAccountManagement.Web.ViewModels.AccountManagement;
+using System.Globalization;
 
 namespace FrontendAccountManagement.Web.UnitTests.ViewModels
 {
@@ -7,12 +8,12 @@ namespace FrontendAccountManagement.Web.UnitTests.ViewModels
     public class DetailsChangeRequestedViewModelTests
     {
 
-       // [TestMethod]
+        [TestMethod]
         public void GetFormattedChangeMessage_ShouldReturnCorrectMessage_ForValidInput()
         {
             // Arrange
             var username = "Dwight Schrute";
-            var updatedDateTime = new DateTime(2024, 6, 25, 12, 6, 0);
+            var updatedDateTime = new DateTime(2024, 6, 25, 12, 6, 0, DateTimeKind.Utc);
             var viewModel = new DetailsChangeRequestedViewModel()
             {
                 Username = username,
@@ -23,34 +24,8 @@ namespace FrontendAccountManagement.Web.UnitTests.ViewModels
             var result = viewModel.GetFormattedChangeMessage("Requested");
 
             // Assert
-            var expectedMessage = "Requested by Dwight Schrute at 12:06pm on 25th June 2024";
+            var expectedMessage = "Requested by Dwight Schrute at 01:06pm on 25th June 2024";
             result.Should().Be(expectedMessage);
-        }
-
-     //   [DataTestMethod]
-        [DataRow("Test User", "2024-06-01", "Requested by Test User at 12:00am on 01st June 2024")]
-        [DataRow("Test User", "2024-06-02", "Requested by Test User at 12:00am on 02nd June 2024")]
-        [DataRow("Test User", "2024-06-03", "Requested by Test User at 12:00am on 03rd June 2024")]
-        [DataRow("Test User", "2024-06-04", "Requested by Test User at 12:00am on 04th June 2024")]
-        [DataRow("Test User", "2024-06-21", "Requested by Test User at 12:00am on 21st June 2024")]
-        [DataRow("Test User", "2024-06-22", "Requested by Test User at 12:00am on 22nd June 2024")]
-        [DataRow("Test User", "2024-06-23", "Requested by Test User at 12:00am on 23rd June 2024")]
-        public void GetFormattedChangeMessage_ShouldReturnCorrectOrdinal_ForDaysWithSpecialSuffixes(
-            string username, string dateString, string expectedMessage)
-        {
-            // Arrange
-            var date = DateTime.Parse(dateString);
-            var viewModel = new DetailsChangeRequestedViewModel
-            {
-                Username = username,
-                UpdatedDatetime = date
-            };
-
-            // Act
-            var result = viewModel.GetFormattedChangeMessage("Requested");
-
-            // Assert
-            Assert.AreEqual(expectedMessage, result);
         }
     }
 }
