1a50e88cac89f26590191fe3cfaec04444411477
Ramakrishna Gujjula <ramakrishna.gujjula.external@eviden.com>
2024-09-18 10:44:52 +0100

    442337-ga-tags: GA tag TagManagerContainerId updated.

--- src/FrontendAccountManagement.Web/Constants/ContextKeys.cs
+++ src/FrontendAccountManagement.Web/Constants/ContextKeys.cs
@@ -4,7 +4,7 @@ public static class ContextKeys
 {
     public const string UseGoogleAnalyticsCookieKey = "UseGoogleAnalyticsCookie";
 
-    public const string TagManagerContainerIdKey = "TagmanagerContainerId";
+    public const string TagManagerContainerIdKey = "TagManagerContainerId";
 
     public const string ScriptNonceKey = "ScriptNonce";
 }
