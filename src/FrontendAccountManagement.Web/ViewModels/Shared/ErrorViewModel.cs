0ec2e886cce0355690702e8609cab41d4a94af10
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-18 21:13:51 +0100

    "dd/MM/yyyy HH:mm:ss"

--- src/FrontendAccountManagement.Web/ViewModels/Shared/ErrorViewModel.cs
+++ src/FrontendAccountManagement.Web/ViewModels/Shared/ErrorViewModel.cs
@@ -4,6 +4,6 @@ namespace FrontendAccountManagement.Web.ViewModels.Shared
 {
     public class ErrorViewModel
     {
-        public string Timestamp { get; set; } = DateTime.UtcNow.UtcToGmt().ToString();
+        public string Timestamp { get; set; } = DateTime.UtcNow.UtcToGmt().ToString("dd/MM/yyyy HH:mm:ss");
     }
 }
