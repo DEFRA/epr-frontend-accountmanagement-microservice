eda9c2c7c4871ccdbfc6d16fab8b13e47938cba1
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-23 13:32:18 +0100

    error page fixes

---
+++ src/FrontendAccountManagement.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace FrontendAccountManagement.Web.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime UtcToGmt(this DateTime dateTime) => TimeZoneInfo.ConvertTimeFromUtc(dateTime, TimeZoneInfo.FindSystemTimeZoneById("Europe/London"));
+    }
+}
