a2168525fc43fe52319de57e2e3e8138d399368f
Paul Shimmen <paul.shimmen@eviden.com>
2025-01-27 14:15:30 +0000

    Adding aria-label for back button

--- src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
+++ src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
@@ -2,8 +2,9 @@
 {
     public static class LocalizerName
     {
-        public const string UpdateOrgNameBackAriaLabel = "UpdateOrgNameBackAriaLabel"; 
+        public const string UpdateOrgNameBackAriaLabel = "UpdateOrgNameBackAriaLabel";
         public const string UpdateOrgAddressBackAriaLabel = "UpdateOrgAddressBackAriaLabel";
         public const string BusinessPostcodeBackAriaLabel = "BusinessPostcodeBackAriaLabel";
+        public const string BusinessAddressBackAriaLabel = "BusinessAddressBackAriaLabel";
     }
 }
