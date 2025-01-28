5bd73a7a4ebdcee5c86b45adbe4914e3bd08254b
Manish Agarwal <167305073+manishagarwalEviden@users.noreply.github.com>
2025-01-28 12:22:30 +0000

    Feature/ad408613 add new org name (#131)
    * add  new page for update organisation name
    
    * Changed AccountController
    
    * merge code
    
    * Unit tests in progress
    
    * add test method
    
    * add user type check
    
    * fix test issue
    
    * tests fixed
    
    * Fixed PR comments
    
    * copied accountmanagementcontroller from feature branch
    
    * AccountManagmentcontroller fixed
    
    * reverted appsettings
    
    * session added in post
    
    * Added test for session update

--- src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
+++ src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
@@ -5,5 +5,6 @@
         public const string UpdateOrgNameBackAriaLabel = "UpdateOrgNameBackAriaLabel"; 
         public const string UpdateOrgAddressBackAriaLabel = "UpdateOrgAddressBackAriaLabel";
         public const string BusinessPostcodeBackAriaLabel = "BusinessPostcodeBackAriaLabel";
+        public const string CompanyNameBackAriaLabel = "CompanyNameBackAriaLabel";
     }
 }
