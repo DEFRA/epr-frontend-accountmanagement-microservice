289c21da97e8c04c33038643519d569404faec4c
Paul Shimmen <167417965+paulshimmen@users.noreply.github.com>
2025-01-30 13:53:35 +0000

    Feature/userstory/408835 select new address integration aria labels (#130)
    * Adding two aria labels
    
    * Adding aria-label for back button

--- src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
+++ src/FrontendAccountManagement.Web/Constants/LocalizerName.cs
@@ -2,9 +2,10 @@
 {
     public static class LocalizerName
     {
-        public const string UpdateOrgNameBackAriaLabel = "UpdateOrgNameBackAriaLabel"; 
+        public const string UpdateOrgNameBackAriaLabel = "UpdateOrgNameBackAriaLabel";
         public const string UpdateOrgAddressBackAriaLabel = "UpdateOrgAddressBackAriaLabel";
         public const string BusinessPostcodeBackAriaLabel = "BusinessPostcodeBackAriaLabel";
         public const string CompanyNameBackAriaLabel = "CompanyNameBackAriaLabel";
+        public const string BusinessAddressBackAriaLabel = "BusinessAddressBackAriaLabel";
     }
 }
