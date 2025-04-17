f04c28103867eca0cb73040cce7faf28cd78e195
Adeel Dar (ext) <adeel.dar.external@eviden.com>
2025-04-17 11:11:02 +0100

    Moved the changes of the new footer under the feature flag

---
+++ src/FrontendAccountManagement.Web.UnitTests/FeatureManagement/FeatureFlagTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Configuration;
+
+namespace FrontendAccountManagement.Web.UnitTests.FeatureManagement;
+
+[TestClass]
+public class FeatureFlagTests
+{
+    private IConfiguration configuration;
+
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void ShowYourFeedbackFooter_Should_Be_True(bool value)
+    {
+        // Arrange
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            {FrontendAccountManagement.Web.Configs.FeatureFlags.ShowYourFeedbackFooter, value.ToString()}
+        };
+
+        configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        // Act
+        bool showYourFeedbackFooter = bool.Parse(configuration[FrontendAccountManagement.Web.Configs.FeatureFlags.ShowYourFeedbackFooter]);
+
+        // Assert
+        showYourFeedbackFooter.Should().Be(value);
+    }
+}
