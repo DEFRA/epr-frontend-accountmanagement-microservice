68a7015fa3fbfb642119ba9f1d00e12415779189
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-09-10 11:00:16 +0100

    Introduce NullGraphService

---
+++ src/FrontendAccountManagement.Web/Extensions/NullGraphService.cs
@@ -0,0 +1,23 @@
+﻿using EPR.Common.Authorization.Services.Interfaces;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrontendAccountManagement.Web.Extensions;
+
+[ExcludeFromCodeCoverage]
+public class NullGraphService : IGraphService
+{
+    public static readonly NullGraphService Empty = new();
+
+    public Task PatchUserProperty(Guid userId, string propertyName, string value, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task<string?> QueryUserProperty(Guid userId, string propertyName, CancellationToken cancellationToken = default)
+    {
+        return null;
+    }
+}
